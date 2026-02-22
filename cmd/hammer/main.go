4a5203a0314acb0451eee0b1f24d8e262c6c5fc6
tekig <Vova-kyzmin@yandex.ru>
2026-02-22 15:22:30 +0300

    added parallel load testing

--- cmd/hammer/main.go
+++ cmd/hammer/main.go
@@ -10,6 +10,10 @@ import (
 	"math/rand"
 	"os"
 	"os/signal"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"syscall"
 	"time"
 
 	"github.com/tekig/clerk/internal/pb"
@@ -19,7 +23,7 @@ import (
 )
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	if err := app(ctx); err != nil {
@@ -30,10 +34,59 @@ func main() {
 
 func app(ctx context.Context) error {
 	host := flag.String("host", "localhost:50051", "target recorder")
+	concurrency := flag.String("concurrency", "1", "number of parallel records")
 	flag.Parse()
 
+	conc, err := strconv.Atoi(*concurrency)
+	if err != nil {
+		return fmt.Errorf("atoi concurrency: %w", err)
+	}
+
+	_ = conc
+
+	h := New(*host)
+
+	var (
+		wg       sync.WaitGroup
+		onceErr  sync.Once
+		causeErr error
+	)
+	for range conc {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			if err := h.hammer(ctx); err != nil {
+				onceErr.Do(func() {
+					causeErr = err
+				})
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	return causeErr
+}
+
+type Hammer struct {
+	host  string
+	total uint64
+	batch time.Time
+	mu    sync.Mutex
+}
+
+func New(host string) *Hammer {
+	return &Hammer{
+		host:  host,
+		batch: time.Now(),
+	}
+}
+
+func (h *Hammer) hammer(ctx context.Context) error {
 	conn, err := grpc.NewClient(
-		*host,
+		h.host,
 		grpc.WithTransportCredentials(
 			insecure.NewCredentials(),
 		),
@@ -45,9 +98,6 @@ func app(ctx context.Context) error {
 
 	client := pb.NewRecorderClient(conn)
 
-	t1 := time.Now()
-	n := 0
-
 	for {
 		select {
 		case <-ctx.Done():
@@ -56,7 +106,7 @@ func app(ctx context.Context) error {
 		}
 
 		id := uuid.New()
-		var b = make([]byte, 4*1024)
+		b := make([]byte, 4*1024)
 		if _, err := rand.Read(b); err != nil {
 			return fmt.Errorf("rand: %w", err)
 		}
@@ -85,13 +135,17 @@ func app(ctx context.Context) error {
 			return fmt.Errorf("create events: %w", err)
 		}
 
-		n++
+		n := atomic.AddUint64(&h.total, 1)
 		if n%1000 == 0 {
-			d := time.Since(t1)
+			h.mu.Lock()
+
+			d := time.Since(h.batch)
 
 			log.Printf("lastBlock=%s (%s), avg=%s, rpc=%f", base64.RawStdEncoding.EncodeToString(id[:]), id.String(), d/time.Duration(1000), 1000/d.Seconds())
 
-			t1 = time.Now()
+			h.batch = time.Now()
+
+			h.mu.Unlock()
 		}
 	}
 }
